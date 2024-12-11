bdac280b5d5f5f6c740e0408e65569dd3706b9dc
rachataa <rachata.arnankul@gmail.com>
2024-12-11 14:35:15 +0700

    seo model

--- server/models/Seo.js
+++ server/models/Seo.js
@@ -4,8 +4,14 @@ const schema = new mongoose.Schema(
   {
     page: { type: String },
     seoDescriptionTH: { type: String },
+    seoDescriptionEN: { type: String },
+    seoDescriptionJP: { type: String },
     seoKeywordTH: { type: String },
+    seoKeywordEN: { type: String },
+    seoKeywordJP: { type: String },
     seoTitleTH: { type: String },
+    seoTitleEN: { type: String },
+    seoTitleJP: { type: String },
   },
   { timestamps: true }
 );
@@ -16,8 +22,14 @@ schema.methods.toJSON = function () {
     id: this._id,
     page: this.page,
     seoDescriptionTH: this.seoDescriptionTH,
+    seoDescriptionEN: this.seoDescriptionEN,
+    seoDescriptionJP: this.seoDescriptionJP,
     seoKeywordTH: this.seoKeywordTH,
+    seoKeywordEN: this.seoKeywordEN,
+    seoKeywordJP: this.seoKeywordJP,
     seoTitleTH: this.seoTitleTH,
+    seoTitleEN: this.seoTitleEN,
+    seoTitleJP: this.seoTitleJP,
     createdAt: this.createdAt,
     updatedAt: this.updatedAt,
   };
